367a4c8064aef57a7335e7f1b81091a4f2d5288c
Ihor <primshyts1@gmail.com>
2025-03-10 11:36:46 +0200

    Homework_4.1

--- Homework_4.1.py
+++ Homework_4.1.py
@@ -4,6 +4,6 @@ Zero_count=List.count(0)
 
 for i in range(Zero_count):
     index=List.index(0)
-    Delete_zero=List.pop(index)
-    List.append(Delete_zero)
+    Delete_Zero=List.pop(index)
+    List.append(Delete_Zero)
 print(List)

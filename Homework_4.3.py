f37e43fb865a145b5b5117db9fc41c9fe5e91d2e
Ihor <primshyts1@gmail.com>
2025-03-10 15:55:22 +0200

    Homework_4.2

---
+++ Homework_4.3.py
@@ -0,0 +1,12 @@
+import random
+
+List=[random.randint(1,9) for i in range(random.randint(3, 10))]
+print(List)
+
+New List=[]
+
+
+
+# for i in range(Len_list):
+#     List=[random.choice(Numbers_for_List)]
+#     print(List)

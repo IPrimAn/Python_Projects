b46d0336659b455c8c830eefe03397120babfbd4
Ihor <primshyts1@gmail.com>
2025-03-14 19:19:57 +0200

    Homework_5.1

--- Homework_5.1.py
+++ Homework_5.1.py
@@ -11,23 +11,25 @@ while True:
 
     your_variable = input('Input your symbols for variable and we will check it: ')
 
-    if your_variable.islower() != True:
-        print("False")
-        continue
-
-    if your_variable[0].isnumeric == True:
-        print("False")
-        continue
-
     for i in your_variable:
         if i in string_punctuation:
             print("False")
             break
-        elif i in keyword.kwlist:
+    for i in your_variable:
+        if your_variable in keyword.kwlist:
             print("False")
-            continue
-        elif i in test_data:
-            print("False")
-        else:
             break
-    print("True")
+    for i in your_variable:
+        if your_variable in test_data:
+            print("False")
+            break
+
+    if your_variable.islower() != True:
+        print("False")
+        break
+
+    else:
+        break
+    if your_variable[0].isnumeric == True:
+        print("False")
+        continue

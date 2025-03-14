32904c235f1e4d87d8e517a440833c5d1f1fee70
Ihor <primshyts1@gmail.com>
2025-03-14 09:40:30 +0200

    Homework_5.3

---
+++ Homework_5.1.py
@@ -0,0 +1,25 @@
+import keyword
+import string
+string_punctuation = ""
+print (string.punctuation)
+for i in string.punctuation:
+    string_punctuation = string.punctuation.replace("_"," ")
+print(string_punctuation)
+# while True:
+#
+# your_variable = input('write name for variable and we will check it: ')
+#
+#     for i in your_variable:
+#         if your_variable.islower() == True:
+#         continue
+#         if your_variable.isnumeric(0) == True:
+#             print("False")
+#             continue
+#         else:
+#             continue
+#         if
+
+
+
+
+
